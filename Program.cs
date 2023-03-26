f16feb0648f0aaac7b0641c1e865bad7540998cf
чистоклетов <nik-chistokletov@yandex.ru>
2023-03-26 14:06:25 +0300

    забиваем второй void и выводим результат на экран

--- Program.cs
+++ Program.cs
@@ -12,3 +12,13 @@ void ArrayWithIf(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayWithIf(array1, array2);
+PrintArray(array2);
